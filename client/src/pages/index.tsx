bc1fe97a9dd5bc90a617cc41a936abb4f82637a2
Yevhen <yevhen.lys@teamvoy@.com>
2023-04-05 18:22:42 +0300

    Setup mui

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.scss'
+import { Button } from '@mui/material'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function Home() {
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
           <div>
+          <Button variant="contained">Contained</Button>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
               target="_blank"
